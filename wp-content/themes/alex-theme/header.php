71c783176502323c0673d065deb1dbf7c0c04884
Alex Holt <alex.holt@tradeshift.com>
2019-07-14 14:51:04 -0700

    Remove g-verification

--- wp-content/themes/alex-theme/header.php
+++ wp-content/themes/alex-theme/header.php
@@ -2,7 +2,6 @@
 <html>
   <head>
     <title>alexholt.me</title>
-    <meta name="google-site-verification" content="Iy_hNrCObdiFhBFKZZT2zpue1i1ObEYuRTv9hDIFNyE"/>
   </head>
   <body>
     <nav>alexholt.me</nav>

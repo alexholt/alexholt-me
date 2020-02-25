899a780747c0e6d721fec81576605158772bb7fd
Alex Holt <me@alexholt.me>
2020-02-24 21:08:06 -0800

    Fix the date and make links underlined

--- wp-content/themes/alex-theme/footer.php
+++ wp-content/themes/alex-theme/footer.php
@@ -1,6 +1,6 @@
     <footer class="footer container">
       <p class="fine-print">
-        Copyright © 2019 Alex Holt
+        Copyright © <?= Date('Y') ?> Alex Holt
         <?php if ( is_user_logged_in() ): ?>
           <a class="fine-print" href="/journal">Journal</a>
         <?php endif; ?>
